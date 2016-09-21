b9f7ddf0dd2e07c09500e779e7aac662decca10f
Charlie Calder <cc9431@bard.edu>
2016-09-21 12:50:38 -0400

    First attempt at Add fragment

--- app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
@@ -2,57 +2,20 @@ package edu.bard.todolist_lab1;
 /*
   Most basic Todo List in one activity, no fragments.
  */
-import java.util.ArrayList;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 
-public class ToDoList extends Activity {
-    public static String TAG = "todolab";
-    private EditText mEditText; // enter todo item
-    private Button mItemButton; // add the item to the list
-    private ArrayList<String> mToDoItems; // list of items
-    private ArrayAdapter<String> aa; // adapter from list to viewlist
-    private ListView mListView;
+
+public class ToDoList extends SingleFragmentActivity {
 
     @Override
-    public void onCreate(Bundle stuff) {
-        super.onCreate(stuff);
-
-        // Inflate your view
-        setContentView(R.layout.main); // Extracts resources, autogenerates R.java from XML file
-
-        // Get references to UI widgets
-        mEditText = (EditText) findViewById(R.id.myEditText);
-        mItemButton = (Button) findViewById(R.id.addButton);
-        mListView = (ListView) findViewById(R.id.myListView);
-
-        // Create the ArrayList and the ArrayAdapter
-        mToDoItems = new ArrayList<String>();
-        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mToDoItems);
-
-        // Bind the listview to the array adapter
-        mListView.setAdapter(aa);
-
-        // Add key listener to add the new todo item
-        // when the middle D-pad button is pressed.
-        mItemButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                mToDoItems.add(0, mEditText.getText().toString());
-                aa.notifyDataSetChanged();
-                mEditText.setText("");
-            }
-        });
-
-        Log.i(TAG, "Entered onCreate");
+    protected Fragment createFragment() {
+        return new AddFragment();
     }
 
+    public static String TAG = "todolab";
 
     protected void onStart() {
         super.onStart();
