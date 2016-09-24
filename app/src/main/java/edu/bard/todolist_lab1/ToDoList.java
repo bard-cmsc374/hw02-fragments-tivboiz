1df461e93c2fe4264ee6ab76cd5bf9f344c43dd7
Charlie Calder <cc9431@bard.edu>
2016-09-24 14:06:39 -0400

    - Merged the two fragments - still having three different issues laid out in Questions.txt

--- app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
+++ app/src/main/java/edu/bard/todolist_lab1/ToDoList.java
@@ -3,15 +3,62 @@ package edu.bard.todolist_lab1;
   Most basic Todo List in one activity, no fragments.
  */
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 
+import java.util.ArrayList;
 
-public class ToDoList extends SingleFragmentActivity {
+
+public class ToDoList extends FragmentActivity {
+
+    public ArrayList<String> mToDoItems;
+    private static final String LIT_ARRAY = "lit";
 
     @Override
-    protected Fragment createFragment() {
-        return new AddFragment();
+    public void onCreate(Bundle stuff) {
+        super.onCreate(stuff);
+        setContentView(R.layout.main);
+
+        AddFragment addfrg = new AddFragment();
+        ListFragment lstfrg = new ListFragment();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction trans = fm.beginTransaction();
+
+        trans.add(R.id.add_container, addfrg, "ADDTAG");
+        trans.add(R.id.list_container, lstfrg, "LISTTAG");
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("item");
+
+        if (stuff != null){
+            mToDoItems = stuff.getStringArrayList(LIT_ARRAY);
+            Log.i(LIT_ARRAY, "Updated");
+        } else {
+            mToDoItems = new ArrayList<String>();
+            Log.i(LIT_ARRAY, "Created");
+        }
+
+        if (message != null){
+            mToDoItems.add(message);
+        }
+
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList("list", mToDoItems);
+        lstfrg.setArguments(bundle);
+
+        trans.commit();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle stuff){
+        super.onSaveInstanceState(stuff);
+        stuff.putStringArrayList(LIT_ARRAY, mToDoItems);
+        Log.i(LIT_ARRAY, "Saving");
     }
 
     public static String TAG = "todolab";
